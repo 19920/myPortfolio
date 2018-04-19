79eef8a0790bf35d2df0ce95ff9f56b53cc05ab1
Kadahizi <jbatgoal@yahoo.fr>
2018-04-19 15:49:05 +0200

    first commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,33 +1,21 @@
 import React,{Component} from 'react';
+import Skill from './skill';
+import './Skills.css';
 
 class Skills extends Component{
   render(){
     const {knowledge} = this.props;
-    const skillsList = knowledge.map(knowledge =>{
-      return(
-        <div >
-        <div>
-          <div className="item-inner card ">
-            <button className="btn btn-success">{knowledge.name}</button>
-            <div className="level card-title text-center">{knowledge.level}</div>
-            </div>
-          </div>
-        </div>
-      )
-
-    })
-
     return(
     <div id="skills" className ="jumbotron container ">
       <section id="skills" className="skills-section section ">
         <h2 className="skills-title text-center"><u>Skills</u></h2>
-        <div className="top-skills">
+        <div className="top-skills skills">
           <h3 className="subtitle">Top skills</h3>
-          <div className="row  item col-12 col-sm-12 mb-8 ">
-            {skillsList}
+          <div className="row  item col-12 col-sm-12 mb-8 knowledge">
+              {knowledge.map(knowledge => <Skill key={knowledge.id} knowledge={knowledge} />)}
           </div>
         </div>
-        
+
       </section>
 
 
--- src/components/Spokenlanguages.js
+++ src/components/Spokenlanguages.js
@@ -1,26 +1,15 @@
 import React,{Component} from'react';
+import Languagelist from './Languagelist';
 class Spokenlanguages extends Component{
   render(){
     const {language} = this.props;
-    const languageList= language.map(language =>{
-      console.log(language.name);
-      return(
-      <div>
-        <div className="item-inner card ">
-          <button className="btn btn-success">{language.id}</button>
-          <button className="btn btn-success">{language.name}</button>
-          <div className="level card-title text-center">{language.level}</div>
-          </div>
-        </div>
-    )
-    })
     return(
       <div id="skills" className ="jumbotron container ">
         <section id="skills" className="skills-section section ">
           <div className="other-skills">
             <h3 className="subtitle mt-5 text-center"><u>Languages</u></h3>
             <div className="row  item col-12 col-sm-12 mb-8 ">
-            {languageList}
+            {language.map(language=><Languagelist key={language.id} language={language} />)}
             </div>
           </div>
         </section>
